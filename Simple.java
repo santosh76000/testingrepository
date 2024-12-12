3966d2f9e163ba4200d5adc747e6efc11bd87e8a
santosh76000 <psantosh76000@gmail.com>
2024-12-12 19:34:18 +0530

    dddd
    rrrr

--- Simple.java
+++ Simple.java
@@ -2,22 +2,21 @@
 java simple program testing in jenkins
 Simple.java
 */
-public class Simple
-{
-	public static void main(String args[])
-	{
+public class Simple {
+	public static void main(String args[]) {
 		System.out.println("Hello Jenkins...");
 		System.out.println("Excellent testing ...");
 		System.out.println("Hello Jenkins..");
 		System.out.println("Auto building the project...");
 		System.out.println("Interesting one.");
 		System.out.println("my goodness");
-		Student student=new Student();
+		Student student = new Student();
 		student.setName("Ashok");
 		student.setAge(22);
 		student.setSubject("Math");
-		System.out.println("Name="+student.getName());
-		System.out.println("Age="+student.getAge());
-		System.out.println("Subject="+student.getSubject());
+		System.out.println("Name=" + student.getName());
+		System.out.println("Age=" + student.getAge());
+		System.out.println("Subject=" + student.getSubject());
+		System.out.println("new line added...");
 	}
 }
