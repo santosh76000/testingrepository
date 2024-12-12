18c4ab60234f1f049578acd88e84a27e5b3f5894
santosh76000 <psantosh76000@gmail.com>
2024-12-12 19:44:43 +0530

    Updated File1

--- Simple.java
+++ Simple.java
@@ -4,20 +4,22 @@ Simple.java
 */
 public class Simple {
 	public static void main(String args[]) {
-		System.out.println("Hello Jenkins...");
-		System.out.println("Excellent testing ...");
-		System.out.println("Hello Jenkins..");
-		System.out.println("Auto building the project...");
-		System.out.println("Interesting one.");
-		System.out.println("my goodness");
-		Student student = new Student();
-		student.setName("Ashok");
-		student.setAge(22);
-		student.setSubject("Math");
-		System.out.println("Name=" + student.getName());
-		System.out.println("Age=" + student.getAge());
-		System.out.println("Subject=" + student.getSubject());
-		System.out.println("new line added...");
-		System.out.println("really good one");
+		// System.out.println("Hello Jenkins...");
+		// System.out.println("Excellent testing ...");
+		// System.out.println("Hello Jenkins..");
+		// System.out.println("Auto building the project...");
+		// System.out.println("Interesting one.");
+		// System.out.println("my goodness");
+		// Student student = new Student();
+		// student.setName("Ashok");
+		// student.setAge(22);
+		// student.setSubject("Math");
+		// System.out.println("Name=" + student.getName());
+		// System.out.println("Age=" + student.getAge());
+		// System.out.println("Subject=" + student.getSubject());
+		// System.out.println("new line added...");
+		// System.out.println("really good one");
+		System.out.println("Simple java program run through GitHub&jenkin");
+
 	}
 }
