c473bae2f74215ff64b48708621046b0a7621331
santosh76000 <psantosh76000@gmail.com>
2024-12-12 19:04:48 +0530

    testingggg

---
+++ Simple.java
@@ -0,0 +1,23 @@
+/*
+java simple program testing in jenkins
+Simple.java
+*/
+public class Simple
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello Jenkins...");
+		System.out.println("Excellent testing ...");
+		System.out.println("Hello Jenkins..");
+		System.out.println("Auto building the project...");
+		System.out.println("Interesting one.");
+		System.out.println("my goodness");
+		Student student=new Student();
+		student.setName("Ashok");
+		student.setAge(22);
+		student.setSubject("Math");
+		System.out.println("Name="+student.getName());
+		System.out.println("Age="+student.getAge());
+		System.out.println("Subject="+student.getSubject());
+	}
+}
---
+++ Student.java
@@ -0,0 +1,32 @@
+public class Student {
+	
+	private String name;
+	private int age;
+	private String subject;
+	
+	public Student() {
+		
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+
+}
